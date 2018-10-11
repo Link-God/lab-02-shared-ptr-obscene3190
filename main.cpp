ef30f9d6d78c2980dc7eb843fdfc161cffb3a4ad
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-10-11 22:08:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 template <typename T>
 class shared_ptr {
     T *m_ptr;
-    size_t *count; //счетчик
-public:
+    size_t *count;
+    public:
     shared_ptr() : m_ptr(nullptr), count(nullptr) {}
     shared_ptr(T *ptr) : m_ptr(ptr), count(new size_t(1)) {}
     ~shared_ptr()
@@ -14,25 +14,27 @@ public:
                 delete count;
                 delete m_ptr;
             }
-            else (*count)--;
+            else {
+                (*count)--;
+            }
         }
     }
-    void reset(T * ptr = nullptr) //переназначение указателя
+    void reset(T * ptr = nullptr)
     {
         shared_ptr(ptr).swap(*this);
     }
-    T* operator->() const { //Возвращает сохраненный указатель
+    T* operator->() const {
         return m_ptr;
     }
-    T& operator* () const {  //Возвращает ссылку на объект, на который указывает сохраненный указатель
+    T& operator* () const {
         return *m_ptr;
     }
-    T * get() const { // геттер
+    T * get() const {
         return m_ptr;
     }
     shared_ptr(shared_ptr<T> & other) : count(other.count), m_ptr(other.m_ptr) {
         if (m_ptr) (*count)++;
-    } // конструктор копирования
+    }
     shared_ptr<T> operator=(shared_ptr<T> const & other) {
         if (this != &other) {
             if (count) {
@@ -40,20 +42,22 @@ public:
                     delete count;
                     delete m_ptr;
                 }
-                else (*count)--;
+                else {
+                    (*count)--;
+                }
             }
             m_ptr = other.m_ptr;
             count = other.count;
             (*count)++;
         }
         return *this;
-    } //оператор присваивания
-    void swap(shared_ptr & b) { //Обменивает содержимое двух указателей
+    }
+    void swap(shared_ptr & b) {
         std::swap(m_ptr, b.m_ptr);
         std::swap(count, b.count);
     }
     size_t use_count() const {
-        if(count) {
+        if (count) {
             return *count;
         }
         else {
